1d74045801dc5214b768148d3e5a77751387da96
Pedro Ernesto Tramontina <pedro.tramontina@gmail.com>
2024-12-02 07:31:42 -0300

    Column adjustment

--- resources/js/Pages/SWStarter/Show.jsx
+++ resources/js/Pages/SWStarter/Show.jsx
@@ -16,28 +16,36 @@ export default function Show({ details, type }) {
             <Head title="SW Starter" />
 
             <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            <div className="font-semibold text-2xl">
-                                {type === "films"
-                                    ? details.title
-                                    : details.name}
-                            </div>
+                <div className="grid grid-cols-8">
+                    <div className="col-start-3 col-span-4">
+                        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                            <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                                <div className="p-6 text-gray-900">
+                                    <div className="font-semibold text-2xl">
+                                        {type === "films"
+                                            ? details.title
+                                            : details.name}
+                                    </div>
 
-                            <div className="flex mb-4">
-                                {type === "films" ? (
-                                    <MovieList details={details}></MovieList>
-                                ) : (
-                                    <PeopleList details={details}></PeopleList>
-                                )}
-                            </div>
-                            <div className="">
-                                <Link href="/sw-starter">
-                                    <PrimaryButton>
-                                        BACK TO SEARCH
-                                    </PrimaryButton>
-                                </Link>
+                                    <div className="flex mb-4">
+                                        {type === "films" ? (
+                                            <MovieList
+                                                details={details}
+                                            ></MovieList>
+                                        ) : (
+                                            <PeopleList
+                                                details={details}
+                                            ></PeopleList>
+                                        )}
+                                    </div>
+                                    <div className="">
+                                        <Link href="/sw-starter">
+                                            <PrimaryButton>
+                                                BACK TO SEARCH
+                                            </PrimaryButton>
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
